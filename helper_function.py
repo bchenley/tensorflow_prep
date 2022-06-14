33eb5b84e69e83c94b0d3ac73ae4551be548b5e9
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2022-06-14 00:19:14 -0400

    Create helper_function.py

---
+++ helper_function.py
@@ -0,0 +1,76 @@
+## Create TB callback
+def create_tb_callback(dir_name, experiment_name):
+  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  tb_callback = tb(log_dir=log_dir)
+  print(f"Saving TB log to '{log_dir}'")
+  return tb_callback
+##
+
+## create a model from URL
+def create_model(model_url, num_classes=2):
+  """
+  Create Sequential from Hub.
+
+  Arguments:
+    model_url (str): A TF Hub feature extraction URL.
+    num_classes (int): Number of output neurons in the output layer (number of target classes).
+
+  Returns:
+    An uncompiled Keras Sequential model with model_url as feature extraction layer
+    and Dense output layer with num_classes output neurons
+  """
+
+  # download pretrained model and save it as a keras layer
+  feature_extraction_layer = hub.KerasLayer(model_url,
+                                            trainable=False, # freeze params
+                                            name="feature_extraction_layer",
+                                            input_shape=IMAGE_SHAPE+(3,))
+  #
+
+  # Create Sequential
+  model = tf.keras.Sequential([feature_extraction_layer,
+                              layers.Dense(num_classes,
+                                          activation = "softmax",
+                                          name="output_layer")])
+
+  return model
+  #
+##
+
+## plot loss curves
+def plot_loss(history,n_fig):
+  """
+  Plot training and validation loss and accuracy.
+
+  Arguments:
+    history: TensorFlow history object.
+  
+  Returns:
+    plots of training/validation loss and accuracy  
+  """
+
+  # data
+  history_ = history.history
+
+  epochs = range(len(history_["loss"]))
+
+  train_loss = history_["loss"]
+  val_loss = history_["val_loss"]
+
+  train_accuracy = history_["accuracy"]
+  val_accuracy = history_["val_accuracy"]
+  #
+
+  fig, ax = plt.subplots(1,2,num=n_fig)
+  # plot (matplotlib)
+  ax[0].plot(epochs, train_loss,'b' , label="train_loss")
+  ax[0].plot(epochs, val_loss, 'r' , label="val_loss")
+  # ax[0].title('Loss')
+  # ax[0].xlabel('Epochs')
+  ax[1].plot(epochs, train_accuracy,'--b' , label="train_acc")
+  ax[1].plot(epochs, val_accuracy, '--r' , label="val_acc")
+  # ax[1].title('Accuracy')
+  # ax[1].xlabel('Epochs')
+  #
+
+##
