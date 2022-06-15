c526fcd1180073296a623bb4c2e3ad7b713924cb
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2022-06-15 15:32:07 -0400

    Update helper_functions.py

--- tf_prep/helper_functions.py
+++ tf_prep/helper_functions.py
@@ -115,7 +115,7 @@ def image_train_test_from_directory(train_dir,test_dir,image_size,label_mode,bat
 ## create model from base (#1)
 def fit_base_model_1(train_data, test_data, 
                      base_model, base_model_trainable, 
-                     input_shape, norm_inputs, aug,
+                     input_shape, aug,
                      num_outputs, output_activition,
                      loss, optimizer, metrics, 
                      epochs, pct_validate,
@@ -129,43 +129,38 @@ def fit_base_model_1(train_data, test_data,
   inputs = tf.keras.layers.Input(shape=input_shape, name="input_layer")
   #
 
-  # 3. normalize inputs
-  if norm_inputs:
-    inputs = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)(inputs)
-  #
-
-  # 4. augment features
+  # 3. augment features
   if aug:
     inputs = aug(inputs)
   #
   
-  # 5. pass inputs to base model
+  # 4. pass inputs to base model
   h_base = base_model(inputs)
   #
 
   print(f"shape of base model output: {h_base.shape}")
 
-  # 6. Average pool outputs
+  # 5. Average pool outputs
   h_avg = tf.keras.layers.GlobalAveragePooling2D(name="global_average_pooling_layer")(h_base)
   #
 
   print(f"shape of average pooling output: {h_avg.shape}")
 
-  # 7. create output activation layer
+  # 6. create output activation layer
   outputs = tf.keras.layers.Dense(num_outputs, activation=output_activition, name = "output_layer")(h_avg)
   #
 
-  # 8. create main model
+  # 7. create main model
   model = tf.keras.Model(inputs, outputs)
   #
 
-  # 9. compile model
+  # 8. compile model
   model.compile(loss = loss,
                 optimizer = optimizer,
                 metrics = metrics)
   #
 
-  # 10. fit model
+  # 9. fit model
   history = model.fit(train_data,
                       epochs = epochs,
                       steps_per_epoch = len(train_data),
