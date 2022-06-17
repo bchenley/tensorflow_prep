e6ebe6f5135b4885d5bd207c061f049f5d0e7c8e
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2022-06-17 13:56:25 -0400

    Update helper_functions.py

--- tf_prep/helper_functions.py
+++ tf_prep/helper_functions.py
@@ -126,7 +126,10 @@ def fit_base_model_1(train_data, test_data,
                      callback):
 
   # 1. base model trainabble ?
-  base_model.trainable = base_model_trainable
+  # base_model.trainable = base_model_trainable
+  if base_model_trainable:
+    for i in enumerate(base_model_trainable):
+      base_model.layers[i].trainable = base_model_trainable[i]  
   #
 
   # 2. create inputs into our model
